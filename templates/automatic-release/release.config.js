7d99c52ad8715b9ef7b2d84e5d4a1f917610eca8
Luis Rosales <l.rosales@inpsyde.com>
2024-04-16 09:48:05 +0200

    fix: fixes version name in package main file and commit message

--- templates/automatic-release/release.config.js
+++ templates/automatic-release/release.config.js
@@ -30,12 +30,12 @@ module.exports = {
       "npmPublish": false
     }],
     ["@semantic-release/exec", {
-      "prepareCmd": "[ -f index.php ] || [ -f style.css ] && sed -i \"s/Version:.*/Version:     ${nextRelease.version}/\" index.php style.css || true",
+      "prepareCmd": "[ -f index.php ] || [ -f style.css ] && sed -i \"s/Version:.*/Version:     ${nextRelease.version.replace('-built', '')}/\" index.php style.css || true",
     }],
     "@semantic-release/github",
     ["@semantic-release/git", {
       "assets": ["CHANGELOG.md", "package-lock.json", "package.json", "composer.json", "style.css", "index.php"],
-      "message": "chore(release): \${nextRelease.version} [skip ci]\n\n\${nextRelease.notes}"
+      "message": "chore(release): \${nextRelease.version.replace('-built', '')} [skip ci]\n\n\${nextRelease.notes}"
     }]
   ],
   "preset": "angular",
